230cb73f3168a9acdc3cf8ff32185ac750c89114
Loc Nguyen <nguyenloc300796@gmail.com>
2020-06-16 05:31:26 +0700

    Add basic calculations of leap year

---
+++ calculate.py
@@ -0,0 +1,30 @@
+def is_leap_year(year):
+    """
+    Check if input `year` is a leap year
+    Input:
+    - year (int)
+    Output:
+    - leap (boolean): True if is leap year, False otherwise
+    """
+    if year % 4 == 0:
+        if year % 100 == 0:
+            if year % 400 == 0:
+                return True
+            return False
+        return True
+    return False
+
+
+def next_five_leap(year):
+    """Return the next five leap years after input `year`
+    Input:
+    - year (int)
+    Output:
+    - result (list of int): list of 5 years that are leap years
+    """
+    result = []
+    while len(result) < 5:
+        year += 1
+        if is_leap_year(year):
+            result.append(year)
+    return result
